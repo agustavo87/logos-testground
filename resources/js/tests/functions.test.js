20821894995f6d7211276bb37997889d777c1c4b
Gustavo Ayala <agustavo87@gmail.com>
2020-12-07 11:21:23 -0300

    Se agregó jest y un test de iJSON

---
+++ resources/js/tests/functions.test.js
@@ -0,0 +1,20 @@
+import { iJSON } from "./../Logos/utils/functions";
+
+let okJSON = '{"prop1":"value", "prop2":34}';
+let wrongJSON = '{"prop1:"value", "prop2":34}';
+
+describe('improved JSON parsing interface', () => {
+    it('parses json correctly', () => {
+        let ijson = iJSON(okJSON);
+        expect(ijson.errored).toBeFalsy();
+        expect(ijson.get).toBeInstanceOf(Object);
+    });
+    
+    it('detects json error', () => {
+        let ijson = iJSON(wrongJSON);
+        expect(ijson.get).toBeNull();
+        expect(ijson.errored).toBeTruthy();
+        expect(ijson.error).toBeInstanceOf(SyntaxError);
+    })
+})
+
